a3501ee7df88234f1814dd524ea200e0427f7e83
GeorgiRadevGeorgiev <go6o_mamuta@abv.bg>
2016-11-05 17:59:49 +0200

    Add files via upload

--- gosho ralta 5/gosho ralta 5.cpp
+++ gosho ralta 5/gosho ralta 5.cpp
@@ -6,10 +6,12 @@
 
 using namespace std;
 
-/*
-int main()
 
+int main()
 {
+/*
+ // zadacha 1
+
 	int num;
 	cout << "Input number = ";
 	cin >> num;
@@ -32,6 +34,10 @@ int main()
 		cout << "error , input number between 5 and 20" << endl;
 */
 	
+		// vtora zadacha e pravena predi
+
+   // zadacha 3 
+
 /*
 int main ()
 {
@@ -53,39 +59,125 @@ int main ()
 
 */
 
-/*                                                    //???????????????????????????????????
+
+     // zadacha 4
+                                                    //???????????????????????????????????
+/*
 
 int main ()
 {
 	int n, num;
 	cout << "how many numbers? : ";
 	cin >> n;
+	int low = -n;
 
-	for (int a = 0, low = 0; a < n; a++) {
+	for (int a = 0; a < n; a++) {
 		cout << "\nenter number " << a + 1  << ": ";
 		cin >> num;
-		(num > low && num < 0 ? low = num : 0 );
-		cout << "\nfinal number : " << low << endl;
+
+		if (num > low && num < 0)
+			low = num;
+
+		
 	}
-	
-	*/	
+	cout << "\nfinal number : " << low << endl;
+	*/
 
+  // zadacha 5
 
+/* 
 int main()
 { 
 	int n;
 	cout << " Input n to calculate n! : ";
 	cin >> n;
-	cout << "\nn! = "; 
+	
 	unsigned long long fact = 1;
 
 	for (int a = 1; a <= n; a++) {	
 		fact *= a;
-		      cout << fact << endl;                                                  
+		                                                       
+	}
+	cout << "\nn! = "; 
+	cout << fact << endl; 
+*/
+
+   // zadacha 6
+
+/* 
+	int chlen = 0, broi; 
+	cout << "Input n : ";
+	cin >> broi;
+
+	if (broi >= 0) {
+		for (int i = 1; i <= broi; i++) {
+			cout << "\na" << i << " = " << (i*i + 3 * i);
+
+		}
+		cout << endl;
+	}
+	else
+		cout << "n must be 1 or more !" << endl;
+
+*/
+
+ // zadacha 7 
+
+ /* 
+	int n;
+	cout << "Input how many rows : ";
+	cin >> n;
+	char x, y;
+	cout << "\nInput symbol 1 : ";
+	cin >> x;
+	cout << "\nInput symbol 2 : ";
+	cin >> y;
+
+
+	if (n > 0) {
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = 1; j <= i; j++) {
+				cout << x;
+			}
+
+			for (int k = n; k >= i; k--) {
+				cout << y;
+			}
+
+			cout << endl;
+		}
+		
+	}
+	else
+		cout << "\nThe rows must be 1 or more !" << endl;
+*/
+
+  // zadacha 8 
+
+
+/* 
+	unsigned long long f1 = 0, f2 = 1, n, brat = 0, counter = 0;
+	cout << "Input n : ";
+	cin >> n;
+	 
+	if (n >= 1) {
+		while (counter < n) {
+			brat = f1;
+			f1 = f2;
+			f2 = brat + f2;
+			counter++;
+		}
+	cout << f1 <<endl;
+	} 
+
+	else {
+        cout << "error!";
 	}
 
-	
 
+	*/	
+	
     system("pause");
     return 0;
 }
